75442693b6f2892d515db653f8e66765b6636986
zhixin <wenzhixin2010@gmail.com>
2014-06-24 15:11:58 +0800

    Add zh-TW locale.

--- src/locale/bootstrap-table-zh-TW.js
+++ src/locale/bootstrap-table-zh-TW.js
@@ -7,16 +7,16 @@
 
     $.extend($.fn.bootstrapTable.defaults, {
         formatRecordsPerPage: function(pageNumber) {
-            return '每页显示 ' + pageNumber + ' 条记录';
+            return '每頁顯示 ' + pageNumber + ' 條記錄';
         },
         formatShowingRows: function(pageFrom, pageTo, totalRows) {
-            return '显示第 ' + pageFrom + ' 到第 ' + pageTo + ' 条记录，总共 ' + totalRows + ' 条记录';
+            return '顯示第 ' + pageFrom + ' 到第 ' + pageTo + ' 條記錄，總共 ' + totalRows + ' 條記錄';
         },
         formatSearch: function() {
             return '搜索';
         },
         formatNoMatches: function() {
-            return '没有找到匹配的记录';
+            return '沒有找到匹配的記錄';
         }
     });
 })(jQuery);
