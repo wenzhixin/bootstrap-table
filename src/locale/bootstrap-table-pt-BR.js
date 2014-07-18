c1c803a8715f7730a0684ca1367153f212fa010e
egcerqueira <egcerqueira@gmail.com>
2014-07-18 18:10:15 -0300

    bootstrap-table-pt-BR.js
    Brazilian Portuguese translation file.

---
+++ src/locale/bootstrap-table-pt-BR.js
@@ -0,0 +1,25 @@
+/**
+ * Bootstrap Table Brazilian Portuguese Translation
+ * Author: Eduardo Cerqueira<egcerqueira@gmail.com>
+ */
+(function ($) {
+    'use strict';
+
+    $.extend($.fn.bootstrapTable.defaults, {
+        formatLoadingMessage: function() {
+            return 'Carregando, aguarde…';
+        },
+        formatRecordsPerPage: function(pageNumber) {
+            return pageNumber + ' registros por página';
+        },
+        formatShowingRows: function(pageFrom, pageTo, totalRows) {
+            return 'Exibindo ' + pageFrom + ' até ' + pageTo + ' de ' + totalRows + ' linhas';
+        },
+        formatSearch: function() {
+            return 'Busca'
+        },
+        formatNoMatches: function() {
+            return 'Nenhum registro encontrado';
+        }
+    });
+})(jQuery);
