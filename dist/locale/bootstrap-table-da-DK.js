4902d6e245cffe57cfe7464e5232bbf4585d4789
zhixin <wenzhixin2010@gmail.com>
2014-08-11 16:05:41 +0800

    Run grunt.

---
+++ dist/locale/bootstrap-table-da-DK.js
@@ -0,0 +1,7 @@
+/*
+* bootstrap-table - v1.1.3 - 2014-08-11
+* https://github.com/wenzhixin/bootstrap-table
+* Copyright (c) 2014 zhixin wen
+* Licensed MIT License
+*/
+!function(a){"use strict";a.extend(a.fn.bootstrapTable.defaults,{formatLoadingMessage:function(){return"Indlæser, vent venligst…"},formatRecordsPerPage:function(a){return a+" poster pr side"},formatShowingRows:function(a,b,c){return"Viser "+a+" til "+b+" af "+c+" rækker"},formatSearch:function(){return"Søg"},formatNoMatches:function(){return"Ingen poster fundet"}})}(jQuery);
