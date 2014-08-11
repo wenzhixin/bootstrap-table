be3aded468a40e7829315cf179979aea14bf3323
zhixin <wenzhixin2010@gmail.com>
2014-08-11 16:05:41 +0800

    Run grunt.

---
+++ dist/locale/bootstrap-table-zh-TW.js
@@ -0,0 +1,7 @@
+/*
+* bootstrap-table - v1.1.3 - 2014-08-11
+* https://github.com/wenzhixin/bootstrap-table
+* Copyright (c) 2014 zhixin wen
+* Licensed MIT License
+*/
+!function(a){"use strict";a.extend(a.fn.bootstrapTable.defaults,{formatLoadingMessage:function(){return"正在努力地加載數據中，請稍候……"},formatRecordsPerPage:function(a){return"每頁顯示 "+a+" 條記錄"},formatShowingRows:function(a,b,c){return"顯示第 "+a+" 到第 "+b+" 條記錄，總共 "+c+" 條記錄"},formatSearch:function(){return"搜索"},formatNoMatches:function(){return"沒有找到匹配的記錄"}})}(jQuery);
