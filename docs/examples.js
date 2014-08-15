b1717f4a993ed19e6b603e02969ce9d8b275e276
zhixin <wenzhixin2010@gmail.com>
2014-08-15 09:36:55 +0800

    Add Large Columns example.

--- docs/examples.js
+++ docs/examples.js
@@ -33,3 +33,27 @@ $(function() {
         $('table[data-toggle="table"]').add($('table[id]')).bootstrapTable('resetView');
     });
 });
+
+function buildTable($el, cells, rows) {
+    var i, j, row,
+        columns = [],
+        data = [];
+
+    for (i = 0; i < cells; i++) {
+        columns.push({
+            field: 'field' + i,
+            title: 'Cell' + i
+        });
+    }
+    for (i = 0; i < rows; i++) {
+        row = {};
+        for (j = 0; j < cells; j++) {
+            row['field' + j] = 'Row-' + i + '-' + j;
+        }
+        data.push(row);
+    }
+    $el.bootstrapTable('destroy').bootstrapTable({
+        columns: columns,
+        data: data
+    });
+}
