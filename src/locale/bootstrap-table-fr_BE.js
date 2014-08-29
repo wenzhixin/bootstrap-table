eec34133b8ab59e6454f01be63907d982bec2c4c
Julien Bisconti <julien.bisconti@gmail.com>
2014-08-29 18:42:37 +0200

    adding locale fr_BE

---
+++ src/locale/bootstrap-table-fr_BE.js
@@ -0,0 +1,25 @@
+/**
+ * Bootstrap Table French (Belgium) translation
+ * Author: Julien Bisconti (julien.bisconti@gmail.com)
+ */
+(function ($) {
+    'use strict';
+
+    $.extend($.fn.bootstrapTable.defaults, {
+        formatLoadingMessage: function() {
+            return 'Chargement en cours...';
+        },
+        formatRecordsPerPage: function(pageNumber) {
+            return pageNumber + ' entrées par page';
+        },
+        formatShowingRows: function(pageFrom, pageTo, totalRows) {
+            return 'Affiche de' + pageFrom + ' à ' + pageTo + ' sur ' + totalRows + ' lignes';
+        },
+        formatSearch: function() {
+            return 'Recherche'
+        },
+        formatNoMatches: function() {
+            return 'Pas de fichiers trouvés';
+        }
+    });
+})(jQuery);
