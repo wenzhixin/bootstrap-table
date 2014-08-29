27831fbb1175a5199a4330f3421355781e720a3b
zhixin <wenzhixin2010@gmail.com>
2014-08-29 22:48:53 +0800

    Fix #69: Add bootstrap-table-es_AR.js locale.

---
+++ src/locale/bootstrap-table-es_AR.js
@@ -0,0 +1,25 @@
+/**
+ * Bootstrap Table Spanish (Argentina) translation
+ * Author: Felix Vera (felix.vera@gmail.com)
+ */
+(function ($) {
+    'use strict';
+
+    $.extend($.fn.bootstrapTable.defaults, {
+        formatLoadingMessage: function() {
+            return 'Cargando, espere por favor...';
+        },
+        formatRecordsPerPage: function(pageNumber) {
+            return pageNumber + ' registros por página';
+        },
+        formatShowingRows: function(pageFrom, pageTo, totalRows) {
+            return 'Mostrando ' + pageFrom + ' a ' + pageTo + ' de ' + totalRows + ' filas';
+        },
+        formatSearch: function() {
+            return 'Buscar'
+        },
+        formatNoMatches: function() {
+            return 'No se encontraron registros';
+        }
+    });
+})(jQuery);
