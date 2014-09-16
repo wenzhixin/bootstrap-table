2d78f361482fbf4b724b6f2ed8c637294d448538
gnhaku <gnhaku@gmail.com>
2014-09-16 22:51:46 +0400

    Create bootstrap-table-ru_RU.js

---
+++ src/locale/bootstrap-table-ru_RU.js
@@ -0,0 +1,25 @@
+/**
+ * Bootstrap Table Russian translation
+ * Author: gnhaku <hello@gnhaku.me>
+ */
+(function ($) {
+    'use strict';
+
+    $.extend($.fn.bootstrapTable.defaults, {
+        formatLoadingMessage: function() {
+            return 'Пожалуйста подождите, идет загрузка…';
+        },
+        formatRecordsPerPage: function(pageNumber) {
+            return pageNumber + ' записей на странице';
+        },
+        formatShowingRows: function(pageFrom, pageTo, totalRows) {
+            return 'Показаны страницы с ' + pageFrom + ' по ' + pageTo + ' из ' + totalRows;
+        },
+        formatSearch: function() {
+            return 'Поиск'
+        },
+        formatNoMatches: function() {
+            return 'Записи не найдены';
+        }
+    });
+})(jQuery);
