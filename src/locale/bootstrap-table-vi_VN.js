cd5762c6ea82d9dff84ce6fcee1816453d916f9a
Duc (Dean) N. PHAM <duc.pham@enclave.vn>
2014-09-19 09:21:58 +0700

    Adding locate vi_VN

---
+++ src/locale/bootstrap-table-vi_VN.js
@@ -0,0 +1,25 @@
+/**
+ * Bootstrap Table Vietnamese translation
+ * Author: Duc N. PHAM <pngduc@gmail.com>
+ */
+(function ($) {
+    'use strict';
+
+    $.extend($.fn.bootstrapTable.defaults, {
+        formatLoadingMessage: function() {
+            return 'Đang tải...';
+        },
+        formatRecordsPerPage: function(pageNumber) {
+            return pageNumber + ' bản ghi mỗi trang';
+        },
+        formatShowingRows: function(pageFrom, pageTo, totalRows) {
+            return 'Hiển thị từ trang ' + pageFrom + ' đến ' + pageTo + ' của ' + totalRows + ' bảng ghi';
+        },
+        formatSearch: function() {
+            return 'Tìm kiếm'
+        },
+        formatNoMatches: function() {
+            return 'Không có dữ liệu';
+        }
+    });
+})(jQuery);
