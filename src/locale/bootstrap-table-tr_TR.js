5a24cf840df1bdc047d17cd125620b3f309c5e71
Emin Şen <emnsen@gmail.com>
2014-09-22 11:23:36 +0300

    Create bootstrap-table-tr_TR.js
    Turkish language translation

---
+++ src/locale/bootstrap-table-tr_TR.js
@@ -0,0 +1,25 @@
+/**
+ * Bootstrap Table Turkish translation
+ * Author: Emin Şen
+ */
+(function ($) {
+    'use strict';
+
+    $.extend($.fn.bootstrapTable.defaults, {
+        formatLoadingMessage: function() {
+            return 'Yükleniyor, lütfen bekleyin…';
+        },
+        formatRecordsPerPage: function(pageNumber) {
+            return pageNumber + ' kayıt sayfa başına';
+        },
+        formatShowingRows: function(pageFrom, pageTo, totalRows) {
+            return totalRows + ' kayıttan ' + pageFrom + ' ile ' + pageTo ' arası gösteriliyor'
+        },
+        formatSearch: function() {
+            return 'Ara'
+        },
+        formatNoMatches: function() {
+            return 'Eşleşen kayıt bulunamadı';
+        }
+    });
+})(jQuery);
