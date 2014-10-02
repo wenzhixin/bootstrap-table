5c3269ac670b60d8520babc3e1ec53fb78eb8927
Davide Renzi <davide.renzi@gmail.com>
2014-10-02 11:38:33 +0200

    Create bootstrap-table-it-IT.js
    Italian translation!

---
+++ src/locale/bootstrap-table-it-IT.js
@@ -0,0 +1,25 @@
+/**
+ * Bootstrap Table Italian translation
+ * Author: Davide Renzi<davide.renzi@gmail.com>
+ */
+(function ($) {
+    'use strict';
+
+    $.extend($.fn.bootstrapTable.defaults, {
+        formatLoadingMessage: function() {
+            return 'Caricamento in corso…';
+        },
+        formatRecordsPerPage: function(pageNumber) {
+            return pageNumber + ' records per pagina';
+        },
+        formatShowingRows: function(pageFrom, pageTo, totalRows) {
+            return 'Pagina ' + pageFrom + ' di ' + pageTo + ' (' + totalRows + ' records)';
+        },
+        formatSearch: function() {
+            return 'Cerca'
+        },
+        formatNoMatches: function() {
+            return 'Nessun record trovato';
+        }
+    });
+})(jQuery);
