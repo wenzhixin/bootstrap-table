4354a4c7ccc2185cdf808d67d828bd72170f187f
zhixin <wenzhixin2010@gmail.com>
2014-11-05 22:27:46 +0800

    Update bootstrap-table-editable.js

--- src/extensions/editable/bootstrap-table-editable.js
+++ src/extensions/editable/bootstrap-table-editable.js
@@ -8,7 +8,7 @@
     'use strict';
 
     $.extend($.fn.bootstrapTable.defaults, {
-        editable: false
+        editable: true
     });
 
     var BootstrapTable = $.fn.bootstrapTable.Constructor,
