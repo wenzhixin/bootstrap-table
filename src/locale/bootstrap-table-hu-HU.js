3341e579ad979085cc9009b3e4ebefa78bbab77f
Gergely Nagy <chpdesign@users.noreply.github.com>
2014-12-02 12:59:04 +0100

    Hungarian language file
    I translated the current language file.

---
+++ src/locale/bootstrap-table-hu-HU.js
@@ -0,0 +1,34 @@
+/**
+ * Bootstrap Table Hungarian translation
+ * Author: Nagy Gergely <info@nagygergely.eu>
+ */
+(function ($) {
+    'use strict';
+
+    $.extend($.fn.bootstrapTable.defaults, {
+        formatLoadingMessage: function () {
+            return 'Betöltés, kérem várjon…';
+        },
+        formatRecordsPerPage: function (pageNumber) {
+            return pageNumber + ' rekord per oldal';
+        },
+        formatShowingRows: function (pageFrom, pageTo, totalRows) {
+            return 'Megjelenítve ' + pageFrom + ' - ' + pageTo + ' / ' + totalRows + ' összesen';
+        },
+        formatSearch: function () {
+            return 'Keresés';
+        },
+        formatNoMatches: function () {
+            return 'Nincs találat';
+        },
+        formatRefresh: function () {
+            return 'Frissítés';
+        },
+        formatToggle: function () {
+            return 'Váltás';
+        },
+        formatColumns: function () {
+            return 'Oszlopok';
+        }
+    });
+})(jQuery);
