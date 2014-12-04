7a102f827f470b0179c62eeda8ed9d60f642069e
Ice5050 <ice50505050@hotmail.com>
2014-12-04 16:29:10 +0700

    add prevPage and nextPage public method

--- src/bootstrap-table.js
+++ src/bootstrap-table.js
@@ -1476,6 +1476,16 @@
         }
     };
 
+    BootstrapTable.prototype.prevPage = function () {
+        this.options.pageNumber > 1 ? this.options.pageNumber-- : null;
+        this.updatePagination();
+    };
+
+    BootstrapTable.prototype.nextPage = function () {
+        this.options.pageNumber < this.options.pageSize ? this.options.pageNumber++ : null;
+        this.updatePagination();
+    };
+
     // BOOTSTRAP TABLE PLUGIN DEFINITION
     // =======================
 
@@ -1491,7 +1501,8 @@
         'showLoading', 'hideLoading',
         'showColumn', 'hideColumn',
         'filterBy',
-        'scrollTo'
+        'scrollTo',
+        'prevPage', 'nextPage'
     ];
 
     $.fn.bootstrapTable = function (option, _relatedTarget) {
