c414a85fb39093ebeaf6031314d40516412429bf
zhixin <wenzhixin2010@gmail.com>
2014-12-12 17:54:02 +0800

    grunt 1.5.0

---
+++ docs/dist/locale/bootstrap-table-hu-HU.min.js
@@ -0,0 +1,7 @@
+/*
+* bootstrap-table - v1.5.0 - 2014-12-12
+* https://github.com/wenzhixin/bootstrap-table
+* Copyright (c) 2014 zhixin wen
+* Licensed MIT License
+*/
+!function(a){"use strict";a.fn.bootstrapTable.locales["hu-HU"]={formatLoadingMessage:function(){return"Betöltés, kérem várjon…"},formatRecordsPerPage:function(a){return a+" rekord per oldal"},formatShowingRows:function(a,b,c){return"Megjelenítve "+a+" - "+b+" / "+c+" összesen"},formatSearch:function(){return"Keresés"},formatNoMatches:function(){return"Nincs találat"},formatRefresh:function(){return"Frissítés"},formatToggle:function(){return"Váltás"},formatColumns:function(){return"Oszlopok"}},a.extend(a.fn.bootstrapTable.defaults,a.fn.bootstrapTable.locales["hu-HU"])}(jQuery);
