4c9e2dd7bdd557cf2b10a29b5a40752fdec12fe8
Marco Braga <marco.braga@gmail.com>
2015-02-16 14:28:03 +0100

    Fixed sorting bug when one value is numeric and the other is not.

--- src/bootstrap-table.js
+++ src/bootstrap-table.js
@@ -502,12 +502,8 @@
                     return order * value;
                 }
 
-                // Convert numerical values form string to float.
-                if ($.isNumeric(aa)) {
-                    aa = parseFloat(aa);
-                }
-                if ($.isNumeric(bb)) {
-                    bb = parseFloat(bb);
+                if (value !== undefined) {
+                    return order * value;
                 }
 
                 // Fix #161: undefined or null string sort bug.
@@ -518,7 +514,11 @@
                     bb = '';
                 }
                 
+				// IF both values are numeric, do a numeric comparison
                 if ($.isNumeric(aa) && $.isNumeric(bb)) {
+                    // Convert numerical values form string to float.
+                    aa = parseFloat(aa);
+                    bb = parseFloat(bb);
                     if (aa < bb) {
                         return order * -1;
                     }
@@ -528,6 +528,13 @@
                 if (aa === bb) {
                     return 0;
                 }
+
+				// If value is not a string, convert to string
+                if (typeof aa !== 'string')
+                {
+                    aa = aa.toString();
+                }
+
                 if (aa.localeCompare(bb) === -1) {
                     return order * -1;
                 }
