46c18290d26691d649bf31aa6eede05b2424db84
jtrumbull <jtrumbull@pprnoteco.com>
2015-05-26 14:57:55 -0400

    Removed unnecessary parameter.
    _relatedTarget in $.fn.bootstrapTable

--- src/bootstrap-table.js
+++ src/bootstrap-table.js
@@ -2254,7 +2254,7 @@
         'toggleView'
     ];
 
-    $.fn.bootstrapTable = function (option, _relatedTarget) {
+    $.fn.bootstrapTable = function (option) {
         var value;
 
         this.each(function () {
