5add7ab0bfdf9fdab889c66da661882f3c91383b
Dennis Hernández <dennishernandezvargas@gmail.com>
2015-06-19 14:28:41 -0600

    Improvements

--- src/extensions/group-by/bootstrap-table-group-by.js
+++ src/extensions/group-by/bootstrap-table-group-by.js
@@ -14,9 +14,10 @@
            if (o !== Object(o)) {
                throw new TypeError('Object.keys called on a non-object');
            }
-           var k=[],p;
+           var k = [],
+               p;
            for (p in o) {
-               if (Object.prototype.hasOwnProperty.call(o,p)) {
+               if (Object.prototype.hasOwnProperty.call(o, p)) {
                    k.push(p);
                }
            }
