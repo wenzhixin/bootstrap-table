005ade573d3ec5eb5e2643eb84faf030ddfa4a95
Dan Kaplun <dbkaplun@gmail.com>
2015-07-01 22:57:22 -0400

    Save scroll position

--- dist/extensions/angular/bsTable.js
+++ dist/extensions/angular/bsTable.js
@@ -5,6 +5,7 @@
       restrict: 'EA',
       scope: {options: '='},
       link: function ($s, $el) {
+        var scroll;
         var options;
         $el
           .on('sort.bs.table', function (evt, sortName, sortOrder) {
@@ -19,7 +20,11 @@
           });
         $s.$watch('options', function (newOptions) {
           if (!newOptions) return;
-          if (options) $el.bootstrapTable('destroy');
+
+          if (options) {
+            scroll = $el.bootstrapTable('getScrollPosition');
+            $el.bootstrapTable('destroy');
+          }
           options = $.extend(options ? {
             sortName: options.sortName,
             sortOrder: options.sortOrder,
@@ -27,6 +32,7 @@
             pageSize: options.pageSize
           } : {}, newOptions);
           $el.bootstrapTable(options);
+          if (scroll) $el.bootstrapTable('scrollTo', scroll);
         }, true);
         $(window).resize(function () {
           if (options) $el.bootstrapTable('resetView');
