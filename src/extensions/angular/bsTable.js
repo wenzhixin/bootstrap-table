ab75d58dac3c9ac082dc2ed0c0ada9f4d7b5a328
Dan Kaplun <dbkaplun@gmail.com>
2015-07-01 22:44:21 -0400

    Remember pageNumber, pageSize, sortName, sortOrder

--- src/extensions/angular/bsTable.js
+++ src/extensions/angular/bsTable.js
@@ -1,27 +1,37 @@
 // JavaScript source code
 (function () {
-    angular.module('bsTable', [])
-   .directive('bsTableControl', function () {
-       return {
-           restrict: 'EA',
-           scope: {
-               options: '='
-           },
-           link: function (scope, $el, attr) {
-               var tableCreated = false;
-               scope.$watch('options', function (newValue, oldValue) {
-                   if (tableCreated && newValue === oldValue) return;
-                   $el.bootstrapTable('destroy');
-                   if (newValue) {
-                       $el.bootstrapTable(scope.options);
-                   }
-                   tableCreated = typeof (newValue) !== 'undefined';
-               });
-               $(window).resize(function () {
-                   if (tableCreated)
-                       $el.bootstrapTable('resetView');
-               })
-           }
-       };
-   })
+  angular.module('bsTable', []).directive('bsTableControl', function () {
+    return {
+      restrict: 'EA',
+      scope: {options: '='},
+      link: function ($s, $el) {
+        var options;
+        $el
+          .on('sort.bs.table', function (evt, sortName, sortOrder) {
+            if (!options) return;
+            options.sortName = sortName;
+            options.sortOrder = sortOrder;
+          })
+          .on('page-change.bs.table', function (evt, pageNumber, pageSize) {
+            if (!options) return;
+            options.pageNumber = pageNumber;
+            options.pageSize = pageSize;
+          });
+        $s.$watch('options', function (newOptions) {
+          if (!newOptions) return;
+          if (options) $el.bootstrapTable('destroy');
+          options = $.extend(options ? {
+            sortName: options.sortName,
+            sortOrder: options.sortOrder,
+            pageNumber: options.pageNumber,
+            pageSize: options.pageSize
+          } : {}, newOptions);
+          $el.bootstrapTable(options);
+        }, true);
+        $(window).resize(function () {
+          if (options) $el.bootstrapTable('resetView');
+        })
+      }
+    };
+  })
 })();
