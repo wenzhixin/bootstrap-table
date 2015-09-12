ebec05fdfe5d6e41c4b1736cfb958875beee44c7
Dennis Hernández <dennishernandezvargas@gmail.com>
2015-09-11 21:07:01 -0600

    Added accent neutralise extension

--- src/extensions/multiple-search/bootstrap-table-multiple-search.js
+++ src/extensions/multiple-search/bootstrap-table-multiple-search.js
@@ -8,6 +8,23 @@
 
     'use strict';
 
+    var sprintf = function (str) {
+        var args = arguments,
+            flag = true,
+            i = 1;
+
+        str = str.replace(/%s/g, function () {
+            var arg = args[i++];
+
+            if (typeof arg === 'undefined') {
+                flag = false;
+                return '';
+            }
+            return arg;
+        });
+        return flag ? str : '';
+    };
+
     var getFieldIndex = function (columns, field) {
         var index = -1;
 
