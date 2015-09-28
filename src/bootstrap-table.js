f421c31a44167467d11689d1e705d10450938634
Zirui Wang <renaissance.wangzr@gmail.com>
2015-09-28 18:00:08 -0500

    Bug Fix: selectAll checkbox not reflecting changes
    The current master branch version has a bug that selectAll checkbox not reflecting changes accordingly to highlight all rows in the table.
    I located the problem is there is a missing update for the selection.

--- src/bootstrap-table.js
+++ src/bootstrap-table.js
@@ -790,6 +790,8 @@
             .on('click', '[name="btSelectAll"]', function () {
                 var checked = $(this).prop('checked');
                 that[checked ? 'checkAll' : 'uncheckAll']();
+                
+                that.updateSelected();
             });
     };
 
