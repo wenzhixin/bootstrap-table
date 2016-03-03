0ffd3648463dd31c8227eede06ada9314df7989c
Alec Fenichel <alec.fenichel@gmail.com>
2016-03-02 21:17:30 -0500

    Add support for filter control starts with search

--- src/extensions/filter-control/bootstrap-table-filter-control.js
+++ src/extensions/filter-control/bootstrap-table-filter-control.js
@@ -325,7 +325,8 @@
         filterControl: undefined,
         filterData: undefined,
         filterDatepickerOptions: undefined,
-        filterStrictSearch: false
+        filterStrictSearch: false,
+        filterStartsWithSearch: false
     });
 
     $.extend($.fn.bootstrapTable.Constructor.EVENTS, {
@@ -451,6 +452,13 @@
                         value.toString().toLowerCase() === fval.toString().toLowerCase())) {
                         return false;
                     }
+                }
+                else if(thisColumn.filterStartsWithSearch){
+                  if (!($.inArray(key, that.header.fields) !== -1 &&
+                      (typeof value === 'string' || typeof value === 'number') &&
+                      (value + '').toLowerCase().indexOf(fval) == 0)) {
+                      return false;
+                  }
                 }
                 else {
                     if (!($.inArray(key, that.header.fields) !== -1 &&
