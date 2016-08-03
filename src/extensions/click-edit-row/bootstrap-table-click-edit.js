c98d04da4b9b61c39811d7a2dcd42a1123dd6587
horkenW <horkenwong@gmail.com>
2016-08-03 12:04:55 +0800

    update

--- src/extensions/click-edit-row/bootstrap-table-click-edit.js
+++ src/extensions/click-edit-row/bootstrap-table-click-edit.js
@@ -10,7 +10,7 @@
 
     // var sprintf = $.fn.bootstrapTable.utils.sprintf;
     $.extend($.fn.bootstrapTable.defaults, {
-        clickEdit: true
+        clickEdit: true;
     });
 
     function clikcToEdit(evt){
@@ -19,43 +19,42 @@
             cancel = '<button type="button" class="btn btn-default btn-sm editable-cancel"><i class="glyphicon glyphicon-remove"></i></button>';
       
       var replaceData = function(){
-        txt = [];
-        $(that).find('td').find('input[type="text"]').each(function(i, td){
-            txt.push($(td).eq(0).val());
-            $(td.parentElement.parentElement).text(txt[i]);
-        })
-        table.editing = true;
-        return false;
+          txt = [];
+          $(that).find('td').find('input[type="text"]').each(function(i, td){
+              txt.push($(td).eq(0).val());
+              $(td.parentElement.parentElement).text(txt[i]);
+          })
+          table.editing = true;
+          return false;
       }
 
       var recoveryData = function(){
-        // txt = [];
-        $(that).find('td').find('input[type="text"]').each(function(i, td){
-            $(td.parentElement.parentElement).text(txt[i]);
-        })
-        $('#tooling').remove();
-        table.editing = true;
-        return false;
+          $(that).find('td').find('input[type="text"]').each(function(i, td){
+              $(td.parentElement.parentElement).text(txt[i]);
+          })
+          $('#tooling').remove();
+          table.editing = true;
+          return false;
       }
 
       if(table.editing){
-        table.editing = false;
-        $.each(table.columns, function(i, column){
-          if (!column.editable) return;
+          table.editing = false;
+          $.each(table.columns, function(i, column){
+            if (!column.editable) return;
 
-          var div=$('<div class="editable-input col-md-6 col-sm-6 col-xs-6" style="position: relative;"/>');
-          txt.push($(that).find('td').eq(column.fieldIndex).text()); 
-          div.append($('<input type="text" class="form-control input-sm"/>'));
-          div.append($('<span class="clear"><i class="fa fa-times-circle-o" aria-hidden="true"></i></span>'));
-          $(that).find('td').eq(column.fieldIndex).text('').append(div);
-        });
-        for(var i=0, l=txt.length; i<l; i++){
-            $(that).find('input[type="text"]').eq(i).val(txt[i]);
-        }
-        $(that).find('td').last().append('<div id="tooling" class="editable-buttons"/>');
-        $('.clear').on('click', function(){ $(this).parent().find('input').val('');})
-        $(submit).on('click', replaceData).appendTo('#tooling');
-        $(cancel).on('click', recoveryData).appendTo('#tooling');
+            var div=$('<div class="editable-input col-md-6 col-sm-6 col-xs-6" style="position: relative;"/>');
+            txt.push($(that).find('td').eq(column.fieldIndex).text()); 
+            div.append($('<input type="text" class="form-control input-sm"/>'));
+            div.append($('<span class="clear"><i class="fa fa-times-circle-o" aria-hidden="true"></i></span>'));
+            $(that).find('td').eq(column.fieldIndex).text('').append(div);
+          });
+          for(var i=0, l=txt.length; i<l; i++){
+              $(that).find('input[type="text"]').eq(i).val(txt[i]);
+          }
+          $(that).find('td').last().append('<div id="tooling" class="editable-buttons"/>');
+          $('.clear').on('click', function(){ $(this).parent().find('input').val('');});
+          $(submit).on('click', replaceData).appendTo('#tooling');
+          $(cancel).on('click', recoveryData).appendTo('#tooling');
       }      
     }
 
