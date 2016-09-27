63afef9cc638427ac9aafda58449edb741dfe577
mariobustosjmz <mariobustosjmz@gmail.com>
2016-09-27 15:40:04 -0500

    Fix issue #179 , little change

--- src/bootstrap-table.js
+++ src/bootstrap-table.js
@@ -2361,7 +2361,7 @@
             this.options.totalRows = data[this.options.totalField];
             fixedScroll = data.fixedScroll;
             data = data[this.options.dataField];
-        } else if (!$.isArray(data)) { // support fixedScroll
+        } else if (!$.isArray(data) && data !== null) { // support fixedScroll
             fixedScroll = data.fixedScroll;
             data = data.data;
         }
