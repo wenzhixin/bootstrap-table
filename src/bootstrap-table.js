afa0459982944fdec4e9a832f9c33561c1ffbc6a
Nikita Trukhanovets <n.trukhanovets@gmail.com>
2017-02-20 06:27:35 +0300

    escape column title (#2829)

--- src/bootstrap-table.js
+++ src/bootstrap-table.js
@@ -817,7 +817,7 @@
                 html.push(sprintf('<div class="th-inner %s">', that.options.sortable && column.sortable ?
                     'sortable both' : ''));
 
-                text = column.title;
+                text = that.options.escape ? escapeHTML(column.title) : column.title;
 
                 if (column.checkbox) {
                     if (!that.options.singleSelect && that.options.checkboxHeader) {
