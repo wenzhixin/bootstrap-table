7c725ed686c5b180eed5e512d40bbd4b433be10a
wangshifeng <wsf121116@163.com>
2017-06-12 16:36:05 +0800

    when use bootstrap extensions group, the head width is wrong!

--- src/bootstrap-table.js
+++ src/bootstrap-table.js
@@ -2175,7 +2175,7 @@
         var visibleFields = this.getVisibleFields(),
             $ths = this.$header_.find('th');
 
-        this.$body.find('>tr:first-child:not(.no-records-found) > *').each(function (i) {
+        this.$body.find('>tr::nth-child(2):not(.no-records-found) > *').each(function (i) {
             var $this = $(this),
                 index = i;
 
@@ -2192,6 +2192,7 @@
             }
 
             $th.find('.fht-cell').width($this.innerWidth());
+            $th.css("width", $this.innerWidth());
         });
 
         this.horizontalScroll();
