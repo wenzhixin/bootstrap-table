dff079a0f620be3e48aa618a892bdffdf103b268
taehwa.kim <taehwa.kim@betc.com>
2017-07-12 13:48:07 +0200

    add search html strip option. Default false

--- dist/bootstrap-table.js
+++ dist/bootstrap-table.js
@@ -363,6 +363,7 @@
             detailOpen: 'glyphicon-plus icon-plus',
             detailClose: 'glyphicon-minus icon-minus'
         },
+        searchHtmlStrip: false,
 
         customSearch: $.noop,
 
@@ -1277,8 +1278,10 @@
                         value = item[key];
                     }
 
-                    value = value.replace(/<[^>]*>/gi, '');
-                    console.log(value);
+                    if(that.options.searchHtmlStrip){
+                        value = value.replace(/<[^>]*>/gi, '');    
+                    }
+                    
 
                     if (typeof value === 'string' || typeof value === 'number') {
                         if (that.options.strictSearch) {
