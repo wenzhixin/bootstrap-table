04df836a1fd4b7f644fd927bfa92cd16f04a125c
Paweł Michna <pmichna@gmail.com>
2018-03-15 10:32:52 +0100

    Remove `fixed` option from resizable extension
    According to [colResizable documentation](http://www.bacubacu.com/colresizable/#attributes) the `fixed` option is deprecated. `resizeMode` is the one that should be used.

--- src/extensions/resizable/bootstrap-table-resizable.js
+++ src/extensions/resizable/bootstrap-table-resizable.js
@@ -14,7 +14,6 @@
         //Creates the plugin
         that.$el.colResizable({
             liveDrag: that.options.liveDrag,
-            fixed: that.options.fixed,
             headerOnly: that.options.headerOnly,
             minWidth: that.options.minWidth,
             hoverCursor: that.options.hoverCursor,
@@ -28,7 +27,6 @@
     $.extend($.fn.bootstrapTable.defaults, {
         resizable: false,
         liveDrag: false,
-        fixed: true,
         headerOnly: false,
         minWidth: 15,
         hoverCursor: 'e-resize',
