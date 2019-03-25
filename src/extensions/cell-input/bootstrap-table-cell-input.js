1b091222ea90f6416cca2525cde49fd49a1ac545
Dennis Hernández <dennishernandezvargas@gmail.com>
2019-03-24 23:54:13 -0600

    Remove ;

--- src/extensions/cell-input/bootstrap-table-cell-input.js
+++ src/extensions/cell-input/bootstrap-table-cell-input.js
@@ -16,24 +16,24 @@
         cellInputSelectOptinons: [], // [{ text: '', value: '', disabled: false, default: true },{}]
         cellInputIsDeciaml: false,
         onCellInputInit: function () {
-            return false;
+            return false
         },
         onCellInputBlur: function (field, row, oldValue, $el) {
-            return false;
+            return false
         },
         onCellInputFocus: function (field, row, oldValue, $el) {
-            return false;
+            return false
         },
         onCellInputKeyup: function (field, row, oldValue, $el) {
-            return false;
+            return false
         },
         onCellInputKeydown: function (field, row, oldValue, $el) {
-                return false;
+                return false
         },
         onCellInputSelectChange: function (field, row, oldValue, $el) {
-            return false;
+            return false
         },
-    });
+    })
 
     $.extend($.fn.bootstrapTable.Constructor.EVENTS, {
         'cellinput-init.bs.table': 'onCellInputInit',
@@ -42,38 +42,38 @@
         'cellinput-keyup.bs.table': 'onCellInputKeyup',
         'cellinput-keydown.bs.table': 'onCellInputKeydown',
         'cellinput-selectchange.bs.table': 'onCellInputSelectChange'
-    });
+    })
 
     var BootstrapTable = $.fn.bootstrapTable.Constructor,
         _initTable = BootstrapTable.prototype.initTable,
-        _initBody = BootstrapTable.prototype.initBody;
+        _initBody = BootstrapTable.prototype.initBody
 
     BootstrapTable.prototype.initTable = function () {
-        var that = this;
-        _initTable.apply(this, Array.prototype.slice.apply(arguments));
+        var that = this
+        _initTable.apply(this, Array.prototype.slice.apply(arguments))
 
         // exit if table.options.cellInputEnabled = false
         if (!this.options.cellInputEnabled) {
-            return;
+            return
         }
 
         $.each(this.columns, function (i, column) {
             
             // exit if column.cellInputEnabled = false
             if(!column.cellInputEnabled){
-                return;
+                return
             }
 
             if (column.cellInputType === "text") {
-                var _formatter = column.formatter;
+                var _formatter = column.formatter
                 column.formatter = function (value, row, index) {
-                    var result = _formatter ? _formatter(value, row, index) : value;
+                    var result = _formatter ? _formatter(value, row, index) : value
                     
                     // Решает проблему невозможности ввода кавычек &quot;
-                    result = typeof result === "string" ? result.replace(/"/g, '&quot;') : result;
+                    result = typeof result === "string" ? result.replace(/"/g, '&quot;') : result
 
-                    var isSetDataUniqueIdAttr = column.cellInputUniqueId && column.cellInputUniqueId.length > 0;
-                    var disableCallbackFunc = column.cellInputDisableCallbackFunc;
+                    var isSetDataUniqueIdAttr = column.cellInputUniqueId && column.cellInputUniqueId.length > 0
+                    var disableCallbackFunc = column.cellInputDisableCallbackFunc
                     return ['<input type="text" class="table-td-textbox form-control"',
                         //' id="' + column.field + '"',
                         isSetDataUniqueIdAttr ? ' data-uniqueid="' + row[column.cellInputUniqueId] + '"' : "",
@@ -83,36 +83,36 @@
                         ' autofocus="autofocus"',
                         typeof disableCallbackFunc !== "undefined" && disableCallbackFunc(row) ? ' disabled="disabled"' : '',
                         '>'
-                    ].join('');
-                };
+                    ].join('')
+                }
             } else if (column.cellInputType === "select") {
 
-                var _formatter = column.formatter;
+                var _formatter = column.formatter
                 column.formatter = function (value, row, index) {
                     
-                    var result = _formatter ? _formatter(value, row, index) : value;
-                    var optionDatas = column.cellInputSelectOptinons!= null ? column.cellInputSelectOptinons : [];
-                    var selectoptions = [];
+                    var result = _formatter ? _formatter(value, row, index) : value
+                    var optionDatas = column.cellInputSelectOptinons!= null ? column.cellInputSelectOptinons : []
+                    var selectoptions = []
 
-                    var arrAllowedValues = [];
+                    var arrAllowedValues = []
                     for (var k = 0; k < optionDatas.length; k++) {
-                        arrAllowedValues.push(optionDatas[k].value);
+                        arrAllowedValues.push(optionDatas[k].value)
                     }
-                    var allowedVal = $.inArray(value, arrAllowedValues) >= 0;
+                    var allowedVal = $.inArray(value, arrAllowedValues) >= 0
                     
                     for (var j = 0; j < optionDatas.length; j++) {
-                        var optionData = optionDatas[j];
-                        var isSelected = optionData.value === value;
+                        var optionData = optionDatas[j]
+                        var isSelected = optionData.value === value
                         if (!allowedVal && optionData.disabled) {
-                            isSelected = true;
-                            result = optionData.value;
+                            isSelected = true
+                            result = optionData.value
                         }
 
                         selectoptions.push('<option value="' + optionData.value + '" ' + (isSelected ? ' selected="selected" ' : '') + (optionData.disabled ? ' disabled' : '') + '>' + optionData.text + '</option>');
                     }
 
-                    var isSetDataUniqueIdAttr = column.cellInputUniqueId && column.cellInputUniqueId.length > 0;
-                    var disableCallbackFunc = column.disableCallbackFunc;
+                    var isSetDataUniqueIdAttr = column.cellInputUniqueId && column.cellInputUniqueId.length > 0
+                    var disableCallbackFunc = column.disableCallbackFunc
                     return [
                         '<select class="form-control" style="padding: 4px;"',
                         isSetDataUniqueIdAttr ? ' data-uniqueid="' + row[column.cellInputUniqueId] + '"' : "",
@@ -122,23 +122,23 @@
                         '>',
                         selectoptions.join(''),
                         '</select>'
-                    ].join('');
-                };
+                    ].join('')
+                }
             }else if (column.cellInputType === "textarea") {
 
             } else {
-                return;
+                return
             }
 
-        });
-    };
+        })
+    }
 
     BootstrapTable.prototype.initBody = function (fixedScroll) {
-        var that = this;
-        _initBody.apply(this, Array.prototype.slice.apply(arguments));
+        var that = this
+        _initBody.apply(this, Array.prototype.slice.apply(arguments))
 
         if (!this.options.cellInputEnabled) {
-            return;
+            return
         }
 
         $.each(this.columns, function (i, column) {
@@ -148,11 +148,11 @@
                     var data = that.getData(),
                         index = $(this).parents('tr[data-index]').data('index'),
                         row = data[index],
-                        newValue = $(this).val();
+                        newValue = $(this).val()
 
-                    row[column.field] = newValue;
-                    that.trigger('cellinput-blur', column.field, row, $(this));
-                });
+                    row[column.field] = newValue
+                    that.trigger('cellinput-blur', column.field, row, $(this))
+                })
 
                 that.$body.find('input[data-name="' + column.field + '"]')
                     .off('keyup').on('keyup', function (e) {
@@ -160,11 +160,11 @@
                             index = $(this).parents('tr[data-index]').data('index'),
                             row = data[index],
                             oldValue = row[column.field],
-                            newValue = $(this).val();
+                            newValue = $(this).val()
 
-                        row[column.field] = newValue;
-                        that.trigger('cellinput-keyup', column.field, row, oldValue, index, $(this));
-                    });
+                        row[column.field] = newValue
+                        that.trigger('cellinput-keyup', column.field, row, oldValue, index, $(this))
+                    })
 
                 that.$body.find('input[data-name="' + column.field + '"]')
                     .off('keydown').on('keydown', function (e) {
@@ -172,23 +172,23 @@
                             index = $(this).parents('tr[data-index]').data('index'),
                             row = data[index],
                             oldValue = row[column.field],
-                            newValue = $(this).val();
+                            newValue = $(this).val()
 
                         if (!column.tdtexboxIsDeciaml) {
-                            row[column.field] = newValue;
+                            row[column.field] = newValue
                         }
 
-                        that.trigger('cellinput-keydown', column.field, row, oldValue, index, $(this));
-                    });
+                        that.trigger('cellinput-keydown', column.field, row, oldValue, index, $(this))
+                    })
                 
                 that.$body.find('input[data-name="' + column.field + '"]')
                     .off('focus').on('focus', function (e) {
                         var data = that.getData(),
                             index = $(this).parents('tr[data-index]').data('index'),
-                            row = data[index];
+                            row = data[index]
 
-                        that.trigger('cellinput-focus', column.field, row, $(this));
-                    });
+                        that.trigger('cellinput-focus', column.field, row, $(this))
+                    })
             } else if (column.cellInputType === "select") {
 
                 that.$body.find('select[data-name="' + column.field + '"]')
@@ -198,20 +198,20 @@
                             index = $(this).parents('tr[data-index]').data('index'),
                             row = data[index],
                             oldValue = row[column.field],
-                            newValue = $(this).val();
+                            newValue = $(this).val()
 
-                        var isBoolTrue = newValue.toLowerCase() === 'true';
-                        var isBoolFalse = newValue.toLowerCase() === 'false';
+                        var isBoolTrue = newValue.toLowerCase() === 'true'
+                        var isBoolFalse = newValue.toLowerCase() === 'false'
 
-                        row[column.field] = isBoolTrue ? true : (isBoolFalse) ? false : newValue;
-                        that.trigger('cellinput-selectchange', column.field, row, oldValue, index, $(this));
-                    });
+                        row[column.field] = isBoolTrue ? true : (isBoolFalse) ? false : newValue
+                        that.trigger('cellinput-selectchange', column.field, row, oldValue, index, $(this))
+                    })
             } else if (column.cellInputType === "textarea"){
                 
             }
 
-        });
-        this.trigger('cellinput-init');
-    };
+        })
+        this.trigger('cellinput-init')
+    }
 
-}(jQuery);
+}(jQuery)
