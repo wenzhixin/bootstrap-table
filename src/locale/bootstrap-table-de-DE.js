f073f4c1579d211c319df36cec024a9490277350
Dustin Utecht <me@dustin-utecht.de>
2019-03-25 18:30:20 +0100

    remove old stuff

--- src/locale/bootstrap-table-de-DE.js
+++ src/locale/bootstrap-table-de-DE.js
@@ -11,8 +11,7 @@
       return `${pageNumber} Zeilen pro Seite.`
     },
     formatShowingRows (pageFrom, pageTo, totalRows, totalNotFiltered) {
-      let text =
-      if(totalNotFiltered !== undefined && totalNotFiltered > 0 && totalNotFiltered < totalRows) {
+      if (totalNotFiltered !== undefined && totalNotFiltered > 0 && totalNotFiltered < totalRows) {
         return `Zeige Zeile ${pageFrom} bis ${pageTo} von ${totalRows} Zeile${(totalRows > 1) ? 'n' : ''} (Gefiltert von ${totalNotFiltered} Zeile${(totalNotFiltered > 1) ? 'n' : ''})`
       }
 
