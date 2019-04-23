d6afe8db928def6eb2b3369abb708b584d5e7d20
djhvscf <dennishernandezvargas@gmail.com>
2019-04-22 23:23:10 -0600

    Adding filterOrderBy column option

--- src/utils/index.js
+++ src/utils/index.js
@@ -245,5 +245,48 @@ export default {
       data.push(row)
     })
     return data
+  },
+
+  sort (a, b, order, sortStable) {
+    if (a === undefined || a === null) {
+      a = ''
+    }
+    if (b === undefined || b === null) {
+      b = ''
+    }
+
+    if (sortStable && a === b) {
+      a = a._position
+      b = b._position
+    }
+
+    // If both values are numeric, do a numeric comparison
+    if (this.isNumeric(a) && this.isNumeric(b)) {
+      // Convert numerical values form string to float.
+      a = parseFloat(a)
+      b = parseFloat(b)
+      if (a < b) {
+        return order * -1
+      }
+      if (a > b) {
+        return order
+      }
+      return 0
+    }
+
+    if (a === b) {
+      return 0
+    }
+
+    // If value is not a string, convert to string
+    if (typeof a !== 'string') {
+      a = a.toString()
+    }
+
+    if (a.localeCompare(b) === -1) {
+      return order * -1
+    }
+
+    return order
   }
 }
