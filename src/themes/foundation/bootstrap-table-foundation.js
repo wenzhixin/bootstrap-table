4305a8c115234e1de3d0b06b9f1c922c16125b9a
Dustin Utecht <13292481+UtechtDustin@users.noreply.github.com>
2019-06-06 20:02:00 +0200

    added support (search(clear) button) for foundation

--- src/themes/foundation/bootstrap-table-foundation.js
+++ src/themes/foundation/bootstrap-table-foundation.js
@@ -30,6 +30,10 @@ $.BootstrapTable = class extends $.BootstrapTable {
     this.constants.html.dropdownCaret = '<i class="fa fa-angle-down"></i>'
     this.constants.html.pagination = ['<ul class="pagination%s">', '</ul>']
     this.constants.html.paginationItem = '<li><a class="page-item%s" href="#">%s</a></li>'
+    this.constants.html.inputGroup = '<div class="input-group">%s <div class="input-group-button">%s</div></div>'
+    this.constants.html.searchInput = '<input class="%s input-%s input-group-field" type="text" placeholder="%s">'
+    this.constants.html.searchButton = '<button class="button" type="button" name="search" title="%s"><i class="%s %s"></i></button>'
+    this.constants.html.searchClearButton = '<button class="button" type="button" name="clearSearch" title="%s"><i class="%s %s"></i></button>'
   }
 
   initToolbar () {
