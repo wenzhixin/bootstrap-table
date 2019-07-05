367a38612bc1cd2d0e4b9fb6449da018dbcb2b44
Steven Lietaer <nevets82@gmail.com>
2019-07-05 18:27:18 +0200

    Added bootstrap-table-fr-CH.js and bootstrap-table-fr-LU.js

---
+++ src/locale/bootstrap-table-fr-CH.js
@@ -0,0 +1,91 @@
+/**
+ * Bootstrap Table French (Suisse) translation
+ * Author: Nevets82 <Nevets82@gmail.com>
+ */
+
+$.fn.bootstrapTable.locales['fr-CH'] = {
+  formatLoadingMessage () {
+    return 'Chargement en cours'
+  },
+  formatRecordsPerPage (pageNumber) {
+    return `${pageNumber} lignes par page`
+  },
+  formatShowingRows (pageFrom, pageTo, totalRows, totalNotFiltered) {
+    if (totalNotFiltered !== undefined && totalNotFiltered > 0 && totalNotFiltered > totalRows) {
+      return `Affiche de ${pageFrom} à ${pageTo} sur ${totalRows} lignes (filtrés à partir de ${totalNotFiltered} lignes)`
+    }
+
+    return `Affiche de ${pageFrom} à ${pageTo} sur ${totalRows} lignes`
+  },
+  formatSRPaginationPreText () {
+    return 'page précédente'
+  },
+  formatSRPaginationPageText (page) {
+    return `vers la page ${page}`
+  },
+  formatSRPaginationNextText () {
+    return 'page suivante'
+  },
+  formatDetailPagination (totalRows) {
+    return `Affiche ${totalRows} lignes`
+  },
+  formatClearSearch () {
+    return 'Effacer la recherche'
+  },
+  formatSearch () {
+    return 'Recherche'
+  },
+  formatNoMatches () {
+    return 'Pas de lignes trouvés'
+  },
+  formatPaginationSwitch () {
+    return 'Cacher/Afficher pagination'
+  },
+  formatPaginationSwitchDown () {
+    return 'Afficher pagination'
+  },
+  formatPaginationSwitchUp () {
+    return 'Cacher pagination'
+  },
+  formatRefresh () {
+    return 'Rafraichir'
+  },
+  formatToggle () {
+    return 'Basculer'
+  },
+  formatToggleOn () {
+    return 'Afficher vue carte'
+  },
+  formatToggleOff () {
+    return 'Cacher vue carte'
+  },
+  formatColumns () {
+    return 'Colonnes'
+  },
+  formatColumnsToggleAll () {
+    return 'Tout basculer'
+  },
+  formatFullscreen () {
+    return 'Plein écran'
+  },
+  formatAllRows () {
+    return 'Tout'
+  },
+  formatAutoRefresh () {
+    return 'Rafraîchissement automatique'
+  },
+  formatExport () {
+    return 'Exporter les données'
+  },
+  formatJumpTo () {
+    return 'Aller à'
+  },
+  formatAdvancedSearch () {
+    return 'Recherche avancée'
+  },
+  formatAdvancedCloseButton () {
+    return 'Fermer'
+  }
+}
+
+$.extend($.fn.bootstrapTable.defaults, $.fn.bootstrapTable.locales['fr-CH'])
