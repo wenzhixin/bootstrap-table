6cc47a2fd48337906515548ec2da42945b8521f0
Greg <TantumLabs@users.noreply.github.com>
2020-08-05 13:43:06 -0700

    Checked property ignored
    The checked property is ignored if the field has a value.
    
    For example:
    I am setting the checkbox column to the Id field so there is always a value. If I set the formatter to return { checked: false, disabled: true } the checkbox is still checked because the field has a value.

--- src/bootstrap-table.js
+++ src/bootstrap-table.js
@@ -1454,7 +1454,7 @@ class BootstrapTable {
         type = column.radio ? 'radio' : type
 
         const c = column['class'] || ''
-        const isChecked = (value === true || value_ || (value && value.checked)) && value !== false
+        const isChecked = (typeof(value.checked) !== 'undefined' ? value.checked : (value === true || value_ || value && value.checked) && value !== false)
         const isDisabled = !column.checkboxEnabled || (value && value.disabled)
 
         text = [
