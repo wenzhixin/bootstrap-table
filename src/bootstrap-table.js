e6e5e1253a4fd5fc0349b6d568f74e77f2b4ef96
Dustin Utecht <13292481+UtechtDustin@users.noreply.github.com>
2020-09-14 19:13:23 +0200

    use the main dataset instead of the current one

--- src/bootstrap-table.js
+++ src/bootstrap-table.js
@@ -2676,7 +2676,7 @@ class BootstrapTable {
 
   _toggleCheck (checked, index) {
     const $el = this.$selectItem.filter(`[data-index="${index}"]`)
-    const row = this.data[index]
+    const row = this.options.data[index]
 
     if (
       $el.is(':radio') ||
