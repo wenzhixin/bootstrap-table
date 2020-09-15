6caa5eab761c5c703c152d9486b4cf68a7316bdc
djhvscf <dennishernandezvargas@gmail.com>
2020-09-14 19:53:15 -0600

    Adding new unit test

---
+++ cypress/integration/buttons-align.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+describe('Check Buttons Align functionality', () => {
+  it('Check if buttons align was initialized', () => {
+    cy.visit('./cypress/html/core/buttons-align.html')
+      .get('div.fixed-table-toolbar').should('have.length', 1)
+      .get('div.fixed-table-toolbar > .columns-left').should('have.length', 1)
+  })
+})
