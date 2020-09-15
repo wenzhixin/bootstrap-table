f9c6a3807fe0ba15d2f803e8e6b996b2d4a11adc
djhvscf <dennishernandezvargas@gmail.com>
2020-09-14 22:11:22 -0600

    Adding buttons order unit test

---
+++ cypress/integration/buttons-order.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+describe('Check Buttons order functionality', () => {
+  it('Check if buttons order was initialized', () => {
+    cy.visit('./cypress/html/core/buttons-order.html')
+      .get('div.fixed-table-toolbar').should('have.length', 1)
+      .get('div.fixed-table-toolbar > .columns').should('have.length', 1)
+  })
+
+  it('Check if refresh button is the first one', () => {
+    cy.reload(true)
+      .get('div.fixed-table-toolbar > .columns > button')
+      .first()
+      .invoke('attr', 'name')
+      .should('equal', 'refresh')
+  })
+})
