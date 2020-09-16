7d38b524ebe7a7747ed7e30ceebef0a4912b6f06
zhixin <wenzhixin2010@gmail.com>
2020-09-16 17:51:20 +0800

    Fixed default bug of options.data in multiple tables

--- src/bootstrap-table.js
+++ src/bootstrap-table.js
@@ -407,7 +407,7 @@ class BootstrapTable {
     } else if (type === 'prepend') {
       this.options.data = [].concat(data).concat(this.options.data)
     } else {
-      data = data || this.options.data
+      data = data || [...this.options.data]
       this.options.data = Array.isArray(data) ? data : data[this.options.dataField]
     }
 
