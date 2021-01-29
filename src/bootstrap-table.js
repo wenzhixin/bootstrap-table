e98606794a462d19c054ae21de86d0c0447b4097
Dustin Utecht <13292481+UtechtDustin@users.noreply.github.com>
2021-01-29 16:25:32 +0100

    fixed an issue with negativ numbers while searching for ranges

--- src/bootstrap-table.js
+++ src/bootstrap-table.js
@@ -1040,7 +1040,7 @@ class BootstrapTable {
                 return true
               }
             } else {
-              const largerSmallerEqualsRegex = /(?:(<=|=>|=<|>=|>|<)(?:\s+)?(\d+)?|(\d+)?(\s+)?(<=|=>|=<|>=|>|<))/gm
+              const largerSmallerEqualsRegex = /(?:(<=|=>|=<|>=|>|<)(?:\s+)?(-?\d+)?|(-?\d+)?(\s+)?(<=|=>|=<|>=|>|<))/gm
               const matches = largerSmallerEqualsRegex.exec(s)
               let comparisonCheck = false
 
