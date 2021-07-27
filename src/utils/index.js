665cfd29de0ce00442b626755dc4f17288538469
Dustin Utecht <13292481+UtechtDustin@users.noreply.github.com>
2021-07-27 22:50:08 +0200

    fixed linting

--- src/utils/index.js
+++ src/utils/index.js
@@ -196,6 +196,7 @@ export default {
   regexCompare (value, search) {
     try {
       const regexpParts = search.match(/^\/(.*?)\/([gim]*)$/)
+
       if (value.toString().search(regexpParts ? new RegExp(regexpParts[1], regexpParts[2]) : new RegExp(search, 'gim')) !== -1) {
         return true
       }
