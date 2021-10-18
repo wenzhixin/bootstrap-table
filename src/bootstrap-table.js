94921353e8705ce6763a0ebed615502a3053a926
Dustin Utecht <13292481+UtechtDustin@users.noreply.github.com>
2021-10-18 20:56:13 +0200

    Check if page size "all" is selected

--- src/bootstrap-table.js
+++ src/bootstrap-table.js
@@ -1910,9 +1910,8 @@ class BootstrapTable {
       if (this.options.pagination && this.options.sidePagination === 'server') {
         params.offset = this.options.pageSize === this.options.formatAllRows() ?
           0 : this.options.pageSize * (this.options.pageNumber - 1)
-        params.limit = this.options.pageSize === this.options.formatAllRows() ?
-          this.options.totalRows : this.options.pageSize
-        if (params.limit === 0) {
+        params.limit = this.options.pageSize
+        if (params.limit === 0 || this.options.pageSize === this.options.formatAllRows()) {
           delete params.limit
         }
       }
