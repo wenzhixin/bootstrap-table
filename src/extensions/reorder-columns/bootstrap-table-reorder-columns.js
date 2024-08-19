d60c0d9ea5d881347b97ed35a9e6177fb7663b3c
Vinidamiap <vinidamiaop@gmail.com>
2024-08-19 14:29:25 -0300

    fix(reorder-columns): resolve incorrect column values order with detail-view enabled
    Fixed an issue in the "reorder-columns" extension where columns had incorrect values when `data-detail-view` was set to true. The problem occurred because the code attempted to access a non-existent `field` property in the detail-view column. Added a check to ensure the `field` property exists before assigning the value.

--- src/extensions/reorder-columns/bootstrap-table-reorder-columns.js
+++ src/extensions/reorder-columns/bootstrap-table-reorder-columns.js
@@ -136,7 +136,9 @@ $.BootstrapTable = class extends $.BootstrapTable {
         const sortOrder = {}
 
         table.el.find('th').each((i, el) => {
-          sortOrder[$(el).data('field')] = i
+          if ('field' in el.dataset) {
+            sortOrder[$(el).data('field')] = i
+          }
         })
 
         this.columnsSortOrder = sortOrder
