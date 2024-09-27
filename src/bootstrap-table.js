7bbcb71f08c46535368920d38306d0473e9c49cb
Darc-Charlie <charlesisichei@gmail.com>
2024-09-26 20:04:25 -0500

    Return formatted data for all columns in all rows (#7523)
    * Return formatted data for all columns in all rows
    
    - Ensures the returned array is an array of objects where each object has a property for every valid column
    
    * Update src/bootstrap-table.js
    
    Add padding line
    
    Co-authored-by: 文翼 <wenzhixin2010@gmail.com>
    
    ---------
    
    Co-authored-by: 文翼 <wenzhixin2010@gmail.com>

--- src/bootstrap-table.js
+++ src/bootstrap-table.js
@@ -2464,6 +2464,8 @@ class BootstrapTable {
 
     if (params && params.formatted) {
       return data.map(row => {
+        const formattedColumns = {}
+
         for (const [key, value] of Object.entries(row)) {
           const column = this.columns[this.fieldsColumnsIndex[key]]
 
@@ -2471,9 +2473,10 @@ class BootstrapTable {
             continue
           }
 
-          return Utils.calculateObjectValue(column, this.header.formatters[column.fieldIndex],
+          formattedColumns[key] = Utils.calculateObjectValue(column, this.header.formatters[column.fieldIndex],
             [value, row, row.index, column.field], value)
         }
+        return formattedColumns
       })
     }
 
