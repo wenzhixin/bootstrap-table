5266b7aa5e122bed315d60ead93befac98376c03
Pete Deffendol <pete@deffendol.org>
2025-05-05 19:56:36 -0600

    Handle "&" correctly in unescapeHTML
    You need to unescape `&amp;` last, otherwise a string like `&amp;lt;` would result in `<` instead of `&lt;`.

--- src/utils/index.js
+++ src/utils/index.js
@@ -399,11 +399,11 @@ export default {
       return text
     }
     return text.toString()
-      .replace(/&amp;/g, '&')
       .replace(/&lt;/g, '<')
       .replace(/&gt;/g, '>')
       .replace(/&quot;/g, '"')
       .replace(/&#39;/g, '\'')
+      .replace(/&amp;/g, '&')
   },
 
   removeHTML (text) {
