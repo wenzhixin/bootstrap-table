340543975fc5935f96b4d92878bc4df40e9fdf3d
zhixin <wenzhixin2010@gmail.com>
2025-10-26 20:51:35 +0800

    Refactor navigation to use translation functions

--- site/src/i18n/locales/en.js
+++ site/src/i18n/locales/en.js
@@ -1,4 +1,20 @@
 export default {
+  // Navigation
+  'nav.home': 'Home',
+  'nav.docs': 'Docs',
+  'nav.themes': 'Themes',
+  'nav.examples': 'Examples',
+  'nav.online_editor': 'Online Editor',
+  'nav.news': 'News',
+  'nav.blog': 'Blog',
+  'nav.browse': 'Browse',
+  'nav.theme_light': 'Light',
+  'nav.theme_dark': 'Dark',
+  'nav.theme_auto': 'Auto',
+  'nav.toggle_theme': 'Toggle theme',
+  'nav.version_latest': 'Latest (v{version})',
+
+  // Footer
   'footer.description': 'Bootstrap Table Website is a fork of {fork}.',
   'footer.maintainer': 'This fork is developed and maintained by {user}.',
   'footer.license': 'Currently v{version}, Code licensed {license}, docs {by}.',
