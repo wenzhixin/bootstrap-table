a4da40adc24b2a986068428d66a43402a4dd3e8b
zhixin <wenzhixin2010@gmail.com>
2025-11-02 20:15:20 +0800

    Add Chinese locale support to site

---
+++ site/src/i18n/locales/zh-cn.js
@@ -0,0 +1,91 @@
+export default {
+  // Site
+  'site.description': '一个基于 Bootstrap 的扩展表格插件，与一些最广泛使用的 CSS 框架集成。（支持 Bootstrap、Semantic UI、Bulma、Material Design、Foundation）',
+
+  // Navigation
+  'nav.home': '首页',
+  'nav.docs': '文档',
+  'nav.themes': '主题',
+  'nav.examples': '示例',
+  'nav.online_editor': '在线编辑器',
+  'nav.news': '新闻',
+  'nav.blog': '博客',
+  'nav.browse': '浏览',
+  'nav.theme_light': '浅色',
+  'nav.theme_dark': '深色',
+  'nav.theme_auto': '自动',
+  'nav.toggle_theme': '切换主题',
+  'nav.version_latest': '最新版本（v{version}）',
+  'nav.view_on_github': '在 GitHub 上查看',
+
+  // Sidebar/Docs Navigation
+  'sidebar.getting_started': '入门指南',
+  'sidebar.introduction': '介绍',
+  'sidebar.download': '下载',
+  'sidebar.contents': '目录',
+  'sidebar.usage': '用法',
+  'sidebar.browsers_devices': '浏览器和设备',
+  'sidebar.build_tools': '构建工具',
+  'sidebar.api': 'API',
+  'sidebar.table_options': '表格选项',
+  'sidebar.column_options': '列选项',
+  'sidebar.events': '事件',
+  'sidebar.methods': '方法',
+  'sidebar.localizations': '本地化',
+  'sidebar.extensions': '扩展',
+  'sidebar.vuejs': 'VueJS',
+  'sidebar.browser': '浏览器',
+  'sidebar.component': '组件',
+  'sidebar.faq': '常见问题',
+  'sidebar.about': '关于',
+  'sidebar.overview': '概述',
+  'sidebar.license': '许可证',
+
+  // Footer
+  'footer.description': 'Bootstrap Table 网站是 {fork} 的一个分支。',
+  'footer.maintainer': '此分支由 {user} 开发和维护。',
+  'footer.license': '当前版本 v{version}，代码使用 {license} 许可证，文档使用 {by} 许可证。',
+  'footer.links': '链接',
+  'footer.my_website': '我的网站',
+  'footer.my_repos': '我的仓库',
+  'footer.questions': '问题 / 帮助',
+  'footer.email': '邮箱',
+
+  // Subscribe
+  'subscribe.title': '订阅我们的新闻',
+  'subscribe.email_placeholder': '邮箱地址',
+  'subscribe.button': '订阅',
+
+  // TOC
+  'toc.title': '本页目录',
+
+  // Supports
+  'supports.title': '支持团队',
+  'supports.description': '通过贡献、捐赠和赞助，您让 bootstrap-table 能够持续发展。',
+  'supports.loading': '加载赞助者中...',
+  'supports.error': '加载赞助者失败，请稍后重试。',
+  'supports.platinum_title': '铂金',
+  'supports.gold_title': '黄金',
+  'supports.bronze_title': '青铜',
+  'supports.backer_title': '支持者',
+  'supports.backer_description': '以下<b>支持者</b>是为帮助支持 bootstrap-table 而贡献了不同金额的个人。每一份帮助都很重要，我们感谢即使是微小的贡献。',
+  'supports.backer_link': '成为支持者',
+  'supports.sponsor_title': '赞助商',
+  'supports.sponsor_description': '<b>{title} {sponsors}</b>是那些已承诺向 bootstrap-table 捐赠 ${minimum}{range} 的个人或组织。',
+  'supports.sponsor_link': '成为赞助商',
+  'supports.sponsor_range': ' 到 ${maximum}',
+  'supports.sponsor_or_more': ' 或更多',
+
+  // Home
+  'home.getting_started': '入门指南',
+  'home.download': '下载',
+  'home.current_version': '当前版本 v{version}',
+  'home.installation': '安装',
+  'home.installation_description': '通过 npm 或 yarn 包含 Bootstrap Table 的源 CSS 和 JavaScript 文件。',
+  'home.read_installation_docs': '阅读安装文档',
+  'home.cdn_description': '当您只需要包含 Bootstrap Table 编译后的 CSS 或 JS 时，请使用 {cdnjs}。',
+  'home.explore_docs': '探索文档',
+  'home.examples': '示例',
+  'home.examples_description': 'Bootstrap Table 的示例。',
+  'home.browse_examples': '浏览示例'
+}
