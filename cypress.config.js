8aa90c94c2a143ae382002af3fc1606564e8d7ab
zhixin <wenzhixin2010@gmail.com>
2026-02-24 20:25:37 +0800

    Added comprehensive options cypress tests
    including buttons, card view, checkbox, classes, and click to select features

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'cypress'
-import plugins from './cypress/plugins/index.js'
 
 export default defineConfig({
   video: false,
   screenshot: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents (on, config) {
-      return plugins(on, config)
+    setupNodeEvents () {
+      // No custom plugins needed
     }
   }
 })
--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,12 @@ export default [
         ...globals.browser,
         ...globals.node,
         $: true,
-        jQuery: true
+        jQuery: true,
+        // cypress
+        cy: true,
+        describe: true,
+        expect: true,
+        it: true
       },
       ecmaVersion: 'latest',
       sourceType: 'module'
